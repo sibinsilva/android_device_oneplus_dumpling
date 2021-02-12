5ae66817a79d6160da5e8c26b8b39a03a721bab7
sibinsilva <sibinsilva.com>
2021-02-12 20:12:15 +0000

    dumpling: Add maintainer name

--- kangos_dumpling.mk
+++ kangos_dumpling.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from dumpling device
 $(call inherit-product, device/oneplus/dumpling/device.mk)
 
-PRODUCT_NAME := aosp_dumpling
+PRODUCT_NAME := kangos_dumpling
 
 # Inherit some common aosp stuff.
 $(call inherit-product, vendor/kangos/config/common.mk)
@@ -51,3 +51,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
 
 TARGET_VENDOR := oneplus
+
+#Official Builds
+KANGOS_BUILDTYPE := OFFICIAL
