edbffc43ac6a7146f1b58c956061e50b2d07f075
sibinsilva <sibinsilva@gmail.com>
2021-05-11 21:28:31 +0000

    dumpling: Update FP to redfin May

--- ssos_dumpling.mk
+++ ssos_dumpling.mk
@@ -45,7 +45,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="OnePlus5T-user 10 QKQ1.191014.012 2010292059 release-keys"
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys"
 
 TARGET_VENDOR := oneplus
 
